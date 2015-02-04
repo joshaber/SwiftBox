31a4ee851b1426d9e4e5d7225a72a41b89f9c6e4
joshaber <joshaber@gmail.com>
2015-02-03 19:54:28 -0500

    Update the tests.

--- SwiftBoxTests/SwiftBoxTests.swift
+++ SwiftBoxTests/SwiftBoxTests.swift
@@ -12,17 +12,17 @@ import SwiftBox
 
 class SwiftBoxTests: XCTestCase {
     func testDescription() {
-		let parent = Layout(size: CGSize(width: 300, height: 300),
+		let parent = Node(size: CGSize(width: 300, height: 300),
                     childAlignment: .Center,
                     children: [
-			Layout(flex: 75,
-                   margin: Edges(left: 10, right: 10, top: 0, bottom: 0),
-                   size: CGSize(width: 0, height: 100)),
-			Layout(flex: 25,
-                   size: CGSize(width: 0, height: 50)),
+			Node(flex: 75,
+                 margin: Edges(left: 10, right: 10, top: 0, bottom: 0),
+                 size: CGSize(width: 0, height: 100)),
+			Node(flex: 25,
+                 size: CGSize(width: 0, height: 50)),
 		])
 
-		parent.layout()
-		XCTAssert(parent.description.utf16Count > 0, "Has a description.")
+		let layout = parent.layout()
+		XCTAssert("\(layout)".utf16Count > 0, "Has a description.")
     }
 }
