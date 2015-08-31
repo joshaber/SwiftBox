7780b82717e8fd74bdfde63cce4991c875b4076b
Matias Cudich <mcudich@gmail.com>
2015-08-31 10:08:49 -0700

    Fill out interface

--- SwiftBox/Node.swift
+++ SwiftBox/Node.swift
@@ -13,20 +13,20 @@ public struct Edges {
 	public let right: CGFloat
 	public let bottom: CGFloat
 	public let top: CGFloat
-  public let start: CGFloat
-  public let end: CGFloat
+	public let start: CGFloat
+	public let end: CGFloat
 
 	private var asTuple: (Float, Float, Float, Float, Float, Float) {
 		return (Float(left), Float(top), Float(right), Float(bottom), Float(start), Float(end))
 	}
 
-  public init(left: CGFloat = 0, right: CGFloat = 0, bottom: CGFloat = 0, top: CGFloat = 0, start: CGFloat = 0, end: CGFloat = 0) {
+	public init(left: CGFloat = 0, right: CGFloat = 0, bottom: CGFloat = 0, top: CGFloat = 0, start: CGFloat = 0, end: CGFloat = 0) {
 		self.left = left
 		self.right = right
 		self.bottom = bottom
 		self.top = top
-    self.start = start
-    self.end = end
+		self.start = start
+		self.end = end
 	}
 
 	public init(uniform: CGFloat) {
@@ -34,16 +34,16 @@ public struct Edges {
 		self.right = uniform
 		self.bottom = uniform
 		self.top = uniform
-    self.start = uniform
-    self.end = uniform
+		self.start = uniform
+		self.end = uniform
 	}
 }
 
 public enum FlexDirection: UInt32 {
 	case Column = 0
-  case ColumnReverse = 1
+	case ColumnReverse = 1
 	case Row = 2
-  case RowReverse = 3
+	case RowReverse = 3
 }
 
 public enum Justification: UInt32 {
@@ -70,9 +70,9 @@ public enum SelfAlignment: UInt32 {
 }
 
 public enum Direction: UInt32 {
-  case Inherit = 0
-  case LeftToRight = 1
-  case RightToLeft = 2
+	case Inherit = 0
+	case LeftToRight = 1
+	case RightToLeft = 2
 }
 
 /// A node in a layout hierarchy.
@@ -82,33 +82,37 @@ public struct Node {
 	public static let Undefined: CGFloat = nan("SwiftBox.Node.Undefined")
 
 	public let size: CGSize
-  public let minSize: CGSize
-  public let maxSize: CGSize
+	public let minSize: CGSize
+	public let maxSize: CGSize
 	public let children: [Node]
-  public let flexDirection: FlexDirection
+	public let flexDirection: FlexDirection
 	public let direction: Direction
 	public let margin: Edges
 	public let padding: Edges
+	public let border: Edges
 	public let wrap: Bool
 	public let justification: Justification
 	public let selfAlignment: SelfAlignment
 	public let childAlignment: ChildAlignment
+	public let contentAlignment: ChildAlignment
 	public let flex: CGFloat
 	public let measure: (CGFloat -> CGSize)?
 
-  public init(size: CGSize = CGSize(width: Undefined, height: Undefined), minSize: CGSize = CGSize(width: 0, height: 0), maxSize: CGSize = CGSize(width: Undefined, height: Undefined), children: [Node] = [], flexDirection: FlexDirection = .Column, direction: Direction = .Inherit, margin: Edges = Edges(), padding: Edges = Edges(), wrap: Bool = false, justification: Justification = .FlexStart, selfAlignment: SelfAlignment = .Auto, childAlignment: ChildAlignment = .Stretch, flex: CGFloat = 0, measure: (CGFloat -> CGSize)? = nil) {
+	public init(size: CGSize = CGSize(width: Undefined, height: Undefined), minSize: CGSize = CGSize(width: 0, height: 0), maxSize: CGSize = CGSize(width: Undefined, height: Undefined), children: [Node] = [], flexDirection: FlexDirection = .Column, direction: Direction = .Inherit, margin: Edges = Edges(), padding: Edges = Edges(), border: Edges = Edges(), wrap: Bool = false, justification: Justification = .FlexStart, selfAlignment: SelfAlignment = .Auto, childAlignment: ChildAlignment = .Stretch, contentAlignment: ChildAlignment = .Stretch, flex: CGFloat = 0, measure: (CGFloat -> CGSize)? = nil) {
 		self.size = size
-    self.minSize = minSize
-    self.maxSize = maxSize
+		self.minSize = minSize
+		self.maxSize = maxSize
 		self.children = children
-    self.flexDirection = flexDirection
+		self.flexDirection = flexDirection
 		self.direction = direction
 		self.margin = margin
 		self.padding = padding
+		self.border = border
 		self.wrap = wrap
 		self.justification = justification
 		self.selfAlignment = selfAlignment
 		self.childAlignment = childAlignment
+		self.contentAlignment = contentAlignment
 		self.flex = flex
 		self.measure = measure
 	}
@@ -116,17 +120,19 @@ public struct Node {
 	private func createUnderlyingNode() -> NodeImpl {
 		let node = NodeImpl()
 		node.node.memory.style.dimensions = (Float(size.width), Float(size.height))
-    node.node.memory.style.minDimensions = (Float(minSize.width), Float(minSize.height))
-    node.node.memory.style.maxDimensions = (Float(maxSize.width), Float(maxSize.height))
+		node.node.memory.style.minDimensions = (Float(minSize.width), Float(minSize.height))
+		node.node.memory.style.maxDimensions = (Float(maxSize.width), Float(maxSize.height))
 		node.node.memory.style.margin = margin.asTuple
 		node.node.memory.style.padding = padding.asTuple
+		node.node.memory.style.border = border.asTuple
 		node.node.memory.style.flex = Float(flex)
 		node.node.memory.style.flex_direction = css_flex_direction_t(flexDirection.rawValue)
-    node.node.memory.style.direction = css_direction_t(direction.rawValue)
+		node.node.memory.style.direction = css_direction_t(direction.rawValue)
 		node.node.memory.style.flex_wrap = css_wrap_type_t(wrap ? 1 : 0)
 		node.node.memory.style.justify_content = css_justify_t(justification.rawValue)
 		node.node.memory.style.align_self = css_align_t(selfAlignment.rawValue)
 		node.node.memory.style.align_items = css_align_t(childAlignment.rawValue)
+		node.node.memory.style.align_content = css_align_t(contentAlignment.rawValue)
 		if let measure = measure {
 			node.measure = measure
 		}
@@ -139,7 +145,7 @@ public struct Node {
 		let node = createUnderlyingNode()
 
 		if let maxWidth = maxWidth {
-      node.layoutWithMaxWidth(maxWidth, parentDirection: node.node.memory.style.direction)
+			node.layoutWithMaxWidth(maxWidth, parentDirection: node.node.memory.style.direction)
 		} else {
 			node.layout()
 		}
